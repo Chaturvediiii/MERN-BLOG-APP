ed6237bf709fc314370a20cbef5a1ce08a9044b3
Tanisha Chaturvedi <tanisha.17012003@gmail.com>
2024-03-14 17:59:04 +0530

    Header section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,17 +10,21 @@ import About from '../src/pages/About'
 import Dashboard from '../src/pages/Dashboard'
 import SignUp from '../src/pages/SignUp'
 import SignIn from '../src/pages/SignIn'
+import Header from './components/Header';
+import Projects from '../src/pages/Projects';
 
 
 export default function App() {
   return (
     <Router>
+      <Header/>
       <Routes>
         <Route path='/'  element={<Home/>}/>
         <Route path='/dashboard'  element={<Dashboard/>}/>
         <Route path='/signup'  element={<SignUp/>}/>
         <Route path='/signin'  element={<SignIn/>}/>
         <Route path='/about'  element={<About/>}/>
+        <Route path='/projects'  element={<Projects/>}/>
       </Routes>
     </Router>
   )
