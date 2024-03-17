3e6ea910a640938f98dc865f96f94ba2b72b2c99
Tanisha Chaturvedi <tanisha.17012003@gmail.com>
2024-03-17 14:28:31 +0530

    add preinstallations

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ import authRoutes from './routes/auth.route.js'
 import postRoutes from './routes/post.route.js'
 import commentRoutes from './routes/comment.route.js'
 import cookieParser from 'cookie-parser';
+import path from 'path'
 
 const PORT = process.env.PORT
 
@@ -20,6 +21,9 @@ mongoose
     console.log(err);
 })
 
+
+const __dirname = path.resolve();
+
 const app = express();
 app.use(express.json())
 app.use(cookieParser())
@@ -29,6 +33,12 @@ app.use('/api/user',userRoutes);
 app.use('/api/auth',authRoutes);
 app.use('/api/post', postRoutes);
 app.use('/api/comment', commentRoutes);
+
+app.use(express.static(path.join(__dirname,'/client/dist')))
+
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'client','dist','index.html'))
+})
 
 app.use((err,req,res,next)=>{
     const statusCode = err.statusCode || 500;
